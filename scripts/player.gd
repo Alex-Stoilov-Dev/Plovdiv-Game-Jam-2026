fc70e9f29895c91e8cddf4eebe171fe1c8eb6579
Gamze <gamzesherif2020@gmail.com>
2026-01-31 20:15:19 +0200

    Finalized level 2

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,41 +1,50 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-var can_double_jump = true
-var can_dash = false
-var jumps = 2
+const SPEED = 100
+const JUMP_VELOCITY = -300.0
 
-func unlock_ability(ability_name: String):
-	if ability_name == "double_jump":
-		can_double_jump = true
+var can_double_jump = false
+var can_dash = true  
+var jumps = 2;
+
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	
+	# 1. Add gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else:
+		jumps = 2
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		print(jumps)
-  		velocity.y = JUMP_VELOCITY
-		if Input.is_action_just_pressed("ui_accept") and not is_on_floor() and jumps > 0:
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():
+			# Normal First Jump
 			velocity.y = JUMP_VELOCITY
-		
+			jumps -= 1 
+		elif jumps > 0 and can_double_jump:
+			# Double Jump (in the air)
+			velocity.y = JUMP_VELOCITY
+			jumps -= 1
+			print("Double jumped! Jumps left: ", jumps)
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("move_left", "move_right")
+	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
 
 	move_and_slide()
 
 
-
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.has_node(^"/root/area_mask_1"):
-		can_double_jump = true
+func _on_area_2d_area_entered(area: Area2D):
+	if area.name == "area_mask_1":
+		can_double_jump = true;
