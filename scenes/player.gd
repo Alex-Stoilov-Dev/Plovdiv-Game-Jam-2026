cae4edf083263eb8f93d9a974b2626bea6c0668a
Alexander Stoilov <aleksandarstoilov26@gmail.com>
2026-01-31 13:42:38 +0200

    added double jump, and mask pickup functionality

--- scenes/player.gd
+++ scenes/player.gd
@@ -4,15 +4,31 @@ extends CharacterBody2D
 const SPEED = 100
 const JUMP_VELOCITY = -300.0
 
+var can_double_jump = false
+var can_dash = true  
+var jumps = 2;
+
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	
+	# 1. Add gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else:
+		jumps = 2
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():
+			# Normal First Jump
+			velocity.y = JUMP_VELOCITY
+			jumps -= 1 
+		elif jumps > 0 and can_double_jump:
+			# Double Jump (in the air)
+			velocity.y = JUMP_VELOCITY
+			jumps -= 1
+			print("Double jumped! Jumps left: ", jumps)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -21,5 +37,14 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
 
 	move_and_slide()
+
+
+func _on_area_2d_area_entered(area: Area2D):
+	if area.name == "Area2D":
+		can_double_jump = true;
