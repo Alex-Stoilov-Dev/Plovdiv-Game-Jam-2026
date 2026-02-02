f328ac998edbd9c3919559dc2a31119d688379e1
Alexander Stoilov <aleksandarstoilov26@gmail.com>
2026-02-02 22:30:41 +0200

    Refactored Player Controller. Added animations to all movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,117 +1,191 @@
 extends CharacterBody2D
 
+const SPEED = 150
+const JUMP_VELOCITY = -320.0
+const DASH_SPEED = 400
 
-const SPEED = 120
-const JUMP_VELOCITY = -310.0
-
-const DASH_SPEED = 900.0
+var can_dash = false
+var mask_acquired_1 = false
+var mask_acquired_2 = false
+var dash_duration = 0.2
 var dashing = false
-var has_dash_ability = false
-var can_dash = true
-var dash_timer: Timer
-var dash_again_timer: Timer
+var jump_count = 2
 
-var can_double_jump = false
-var jumps = 2;
+enum STATE{
+	IDLE,
+	MOVING,
+	JUMPING,
+	FALLING,
+	DASHING,
+	SECOND_JUMP
+}
 
-func _ready():
-	dash_timer = Timer.new()
-	dash_timer.wait_time = 0.2
-	dash_timer.one_shot = true
-	dash_timer.timeout.connect(_on_dash_time_timeout)
-	add_child(dash_timer)
-	
-	dash_again_timer = Timer.new()
-	dash_again_timer.wait_time = 1.0
-	dash_again_timer.one_shot = true
-	dash_again_timer.timeout.connect(_on_dash_again_timer_timeout)
-	add_child(dash_again_timer)
+enum FACING{
+	LEFT = -1,
+	RIGHT = 1
+}
+
+var active_state = STATE.IDLE
 
 @onready var animated_sprite = $AnimatedSprite2D
-@onready var player = $CharacterBody2D
 
 func _physics_process(delta: float) -> void:
 	
-	# 1. Add gravity
-	if not is_on_floor():
-		if not dashing:
-			velocity += get_gravity() * delta
-			animated_sprite.play("fall")
-	else:
-		jumps = 2
-		
-		if Input.is_action_just_pressed("dash") and can_dash and has_dash_ability:
-			dashing = true
-			can_dash = false
-			$dash_timer.start()
-			$dash_again_timer.start()
-			animated_sprite.play("run")
-			
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept"):
-		if is_on_floor():
-			# Normal First Jump
-			velocity.y = JUMP_VELOCITY
-			animated_sprite.play("jump")
-			jumps -= 1 
-			
-		elif jumps > 0 and can_double_jump:
-			# Double Jump (in the air)
-			animated_sprite.play("double_jump")
-			velocity.y = JUMP_VELOCITY
-			jumps -= 1
+	var direction := Input.get_axis("move_left","move_right")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		if dashing:
-			velocity.x = (-1 if animated_sprite.flip_h else 1) * DASH_SPEED
-			velocity.y = 0
-		else:
+	match active_state:
+		STATE.IDLE:
+			if Input.is_action_just_pressed("jump"):
+				change_state(STATE.JUMPING)
+				
+			if direction:
+				change_state(STATE.MOVING)
+				
+			if not is_on_floor():
+				change_state(STATE.FALLING)
+			
+			if Input.is_action_just_pressed("dash") and can_dash:
+				change_state(STATE.DASHING)
+			
+		STATE.MOVING:
 			velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	if direction > 0:
-		animated_sprite.flip_h = false
-		animated_sprite.play("run")
-	elif direction < 0:
-		animated_sprite.flip_h = true
-		animated_sprite.play("run")
-		
-	if direction > 0 and not is_on_floor():
-		animated_sprite.flip_h = false
-		animated_sprite.play("fall")
-	elif direction < 0 and not is_on_floor():
-		animated_sprite.flip_h = true
-		animated_sprite.play("fall")
-	if direction == 0:
-		animated_sprite.play("idle")
+			if direction:
+				animated_sprite.flip_h = direction < 0
+				
+			if not is_on_floor():
+				change_state(STATE.FALLING)
+				
+			if direction == 0 and not is_on_floor() and jump_count < 1:
+				change_state(STATE.FALLING)
+				
+			if direction == 0 and not is_on_floor() and jump_count > 1:
+				if Input.is_action_just_pressed("jump"):
+					change_state(STATE.SECOND_JUMP)
+			if direction == 0 and is_on_floor():
+				change_state(STATE.IDLE)
+				
+			if Input.is_action_just_pressed("jump"):
+				change_state(STATE.JUMPING)
+				
+			if Input.is_action_just_pressed("dash") and can_dash:
+				change_state(STATE.DASHING)
+			
+		STATE.JUMPING:
+			velocity.x = direction * SPEED
+			velocity += get_gravity() * delta
 
-	if dashing:
-		var dash_dir = direction
-		if dash_dir == 0:
-			dash_dir = -1 if animated_sprite.flip_h else 1
-		
-		velocity.x = dash_dir * DASH_SPEED
-		velocity.y = 0
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+			if direction:
+				animated_sprite.flip_h = direction < 0
+
+			if Input.is_action_just_pressed("dash") and can_dash:
+				change_state(STATE.DASHING)
+			
+			if Input.is_action_just_released("jump") or velocity.y >= 0:
+				if mask_acquired_1:
+					if Input.is_action_just_pressed("jump"):
+						change_state(STATE.SECOND_JUMP)
+					else:
+						velocity.y = 0
+						change_state(STATE.FALLING)
+				else:
+					velocity.y = 0
+					change_state(STATE.FALLING)
+			
+		STATE.FALLING:
+			velocity.x = direction * SPEED
+			velocity += get_gravity() * delta
+			
+			if direction:
+				animated_sprite.flip_h = direction < 0
+
+			if is_on_floor():
+				change_state(STATE.IDLE)
+			
+			if Input.is_action_just_pressed("dash") and can_dash:
+				change_state(STATE.DASHING)
+			if Input.is_action_just_pressed("jump") and jump_count >= 1:
+				change_state(STATE.SECOND_JUMP)
+
+		STATE.DASHING:
+			velocity.y = 0
+			if direction < 0:
+				velocity.x = DASH_SPEED * FACING.LEFT
+			else:
+				velocity.x = DASH_SPEED * FACING.RIGHT
+				
+			if direction == 0: # If we go from idle to dash state we dash in the direction we are facing in idle 
+				if animated_sprite.flip_h == true: # animated_sprite.flip_h returns true if we face left
+					velocity.x = DASH_SPEED * FACING.LEFT
+				else:
+					velocity.x = DASH_SPEED * FACING.RIGHT
+			dash_duration -= delta
+			
+			if dash_duration <= 0: dashing = false
+			
+			if dashing == false and not is_on_floor():
+				change_state(STATE.FALLING)
+			if dashing == false and is_on_floor(): 
+				velocity.x = 0
+				change_state(STATE.IDLE)
+		STATE.SECOND_JUMP:
+			velocity.x = direction * SPEED
+			velocity += get_gravity() * delta
+
+			if direction:
+				animated_sprite.flip_h = direction < 0
+
+			if Input.is_action_just_pressed("dash") and can_dash:
+				change_state(STATE.DASHING)
+			
+			if Input.is_action_just_released("jump") or velocity.y >= 0:
+				velocity.y = 0
+				change_state(STATE.FALLING)
+			
+			if is_on_floor():
+				change_state(STATE.IDLE)
 
 	move_and_slide()
 
-func _on_area_2d_area_entered(area: Area2D):
-	if area.name == "area_mask_1":
-		can_double_jump = true;
-	if area.name == "area_mask_2":
-		can_dash = true;
-		has_dash_ability = true;
+func change_state(to_state: STATE):
+	active_state = to_state
+	
+	match active_state:
+		STATE.IDLE:
+			if mask_acquired_1:
+				jump_count = 2
+			else:
+				jump_count = 1
+			if mask_acquired_2:
+				can_dash = true
+			animated_sprite.play("idle")
+			
+		STATE.JUMPING:
+			jump_count -= 1
+			animated_sprite.play("jump")
+			velocity.y = JUMP_VELOCITY
+		
+		STATE.MOVING:
+			if mask_acquired_1:
+				jump_count = 2
+			else:
+				jump_count = 1
+			animated_sprite.play("run")
+		
+		STATE.FALLING:
+			animated_sprite.play("fall")
+		
+		STATE.DASHING:
+			animated_sprite.play("dash")
+			dash_duration = 0.2
+			dashing = true
+			can_dash = false
+		
+		STATE.SECOND_JUMP:
+			jump_count -= 1
+			animated_sprite.play("double_jump")
+			velocity.y = JUMP_VELOCITY
 
-#make it stop dashing
-func _on_dash_time_timeout() -> void:
-	dashing = false
-
-func _on_dash_again_timer_timeout() -> void:
-	can_dash = true
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.name == "area_mask_1": 
+		mask_acquired_1 = true
+	if area.name == "area_mask_2": mask_acquired_2 = true
